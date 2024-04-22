369d63a8d68100dbb5f123c282871a8aeb3048ac
Gisselle Barrios <gissellebarrios915@gmail.com>
2024-04-22 10:24:57 -0500

    configuracion de la base de datos

--- AGCSBackend/AGCSBackend/settings.py
+++ AGCSBackend/AGCSBackend/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'AGCSBackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'AGCS_importadora',
+        'USER':'usuario_importadora',
+        'PASSWORD':'123456789',
+        'HOST':'localhost',
+        'PORT':'5432'
     }
 }
 
